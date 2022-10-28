606537f31f46aececf26cb1122c32415f9688476
Harlan Howe <hhowe7@gatech.edu>
2022-10-28 14:21:45 -0500

    Some extra comments... will they be committed/pushed?

--- src/HiFi.java
+++ src/HiFi.java
@@ -1,5 +1,7 @@
 public class HiFi
 {
+    // Adding a test message.
+
     private int volume;
     private float FM_station;
     private float balance;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main
 {
+    // adding a main comment.
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
